c1f7befd03b981d8553d6a0d3a5b509eec45e466
sjy <873458912@qq.com>
2019-04-22 18:52:27 +0800

    测试

--- eladmin-system/src/main/java/me/zhengjie/modules/system/repository/UserRepository.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/repository/UserRepository.java
@@ -40,6 +40,7 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
     @Query(value = "update user set password = ?2 , last_password_reset_time = ?3 where username = ?1",nativeQuery = true)
     void updatePass(String username, String pass, Date lastPasswordResetTime) ;
 
+
     /**
      * 修改头像
      * @param username
