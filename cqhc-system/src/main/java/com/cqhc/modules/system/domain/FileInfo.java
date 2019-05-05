1c4bef56ee84a600414001190d68c3fdef49fd60
admin <jcy452960030@qq.com>
2019-05-05 12:42:27 +0800

    新增通用文件，增加新增用户时密码MD5转换

--- cqhc-system/src/main/java/com/cqhc/modules/system/domain/FileInfo.java
+++ cqhc-system/src/main/java/com/cqhc/modules/system/domain/FileInfo.java
@@ -1,9 +1,12 @@
 package com.cqhc.modules.system.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
-import java.sql.Timestamp;
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 /**
 * @author huicheng
@@ -17,38 +20,46 @@ public class FileInfo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @ApiModelProperty(notes = "上传不需要，新增需要", required = true)
     private Long id;
 
     @Column(name = "file_name",nullable = false)
+    @ApiModelProperty(notes = "文件名", required = true)
     private String fileName;
 
     @Column(name = "file_path",nullable = false)
+    @ApiModelProperty(notes = "文件路径", required = true)
     private String filePath;
 
     @Column(name = "file_size")
     private String fileSize;
 
     @Column(name = "file_page")
-    private Short filePage;
+    private Integer filePage;
 
     @Column(name = "version",nullable = false)
-    private Short version;
+    @ApiModelProperty(notes = "版本号", required = true)
+    private Integer version;
 
     /**
      * 方便统计
      */
     @Column(name = "link_name",nullable = false)
+    @ApiModelProperty(notes = "关联表名", required = true)
     private String linkName;
 
-    @Column(name = "link_id",nullable = false)
+    @Column(name = "link_id")
+    @ApiModelProperty(notes = "上传不需要，新增需要", required = false)
     private Long linkId;
 
-    @Column(name = "cookie")
-    private String cookie;
+    @Column(name = "session_id")
+    private String sessionId;
 
     @Column(name = "creater",nullable = false)
+    @ApiModelProperty(notes = "创建人", required = true)
     private Long creater;
 
-    @Column(name = "create_time",nullable = false)
+    @CreationTimestamp
+    @Column(name = "create_time")
     private Timestamp createTime;
 }
